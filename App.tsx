fc850b45f208bbaa42eebbaf5611dd98d77ac2d6
Felipe Kruschewsky <9900928@prestadorserv.com.br>
2024-01-12 16:27:15 -0300

    Commit inicial

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#242424' }}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Text>Hello World!</Text> : <View />}
+    </View>
+  );
+}
